b1d89e4204bd08c1e22ef9e10186fc27d70b69a2
yusuftopuz <topuzyusuf.97@gmail.com>
2023-11-05 21:42:01 +0300

    second commit

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# THIS IS MAIN.PY
 
 
 # Press the green button in the gutter to run the script.
